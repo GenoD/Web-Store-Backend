037ab4878e33d374f95fe7d1ab671d842da6dd1e
GenoDiaz <genodiaz@yahoo.com>
2018-11-29 23:53:44 -0800

    Implement item deletion

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -25,6 +25,16 @@ const Mutations = {
         id: args.id
       }
     }, info)
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{ id, title }`);
+    // check if they own that item or have the permission
+
+    // delete item
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
